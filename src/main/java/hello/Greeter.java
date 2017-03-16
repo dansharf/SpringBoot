cc191ac4a67e60507f2684dea5a4b1aa4dd068fb
Danil Sharf <danilsharf.tech@gmail.com>
2017-03-15 22:25:23 -0700

    init

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+package main.java.hello;
+
+/**
+ * Created by danil on 2/16/17.
+ */
+public class Greeter {
+    public String sayHello(){
+        return "Hello world!";
+    }
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package main.java.hello;
+
+/**
+ * Created by danil Sharf on 2/16/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
